33994e904e748b2a6eb2120ec1586ea59022cd41
emmanue-ndenga <emmanuel.ndenga@student.moringaschool.com>
2025-03-26 04:03:34 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+
+Copy
 function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', function() {
+    // Event handler logic would go here
+  });
 }
